8d0c83fa27d895d93f2cf02aebd698b7700582e1
MininMobile <miningamer@gmail.com>
2018-06-03 16:52:46 +0100

    Add bin - Add Run

--- extension.js
+++ extension.js
@@ -2,16 +2,19 @@ const vscode = require('vscode');
 
 function activate(context) {
 	var ruota = {};
+	var output = vscode.window.createTerminal("Ruota");
+
+	ruota.path = vscode.extensions.getExtension("minin.ruota").extensionPath + "\\bin\\ruota.exe";
 
 	ruota.run = vscode.commands.registerCommand("ruota.run", function () {
-		let output = vscode.window.createOutputChannel("Ruota");
-
 		let code = vscode.window.activeTextEditor;
 
 		if (code != undefined) {
-			code = code.document.getText();
+			let path = code.document.fileName.split(".");
+			path = path[path.lenth-1];
 
-			output.appendLine(code);
+			output.show();
+			output.sendText(`${ruota.path} ${path}`)
 		}
 	});
 
