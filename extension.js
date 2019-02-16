3b01799b33eb821366825841270e6eb84f32e257
MininMobile <notminin@pm.me>
2019-02-16 21:09:39 +0000

    update to ruotav2

--- extension.js
+++ extension.js
@@ -1,21 +1,23 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 function activate(context) {
-	var ruota = {};
-	var output = vscode.window.createTerminal("Ruota");
+	let ruota = {};
+	let output = vscode.window.createTerminal("Ruota");
 
 	ruota.path = vscode.extensions.getExtension("minin.ruota").extensionPath + "\\bin\\ruota.exe";
+		ruota.path = ruota.path.replace(/\\/g, "\\\\");
 
 	ruota.run = vscode.commands.registerCommand("ruota.run", function () {
 		let code = vscode.window.activeTextEditor;
 
 		if (code != undefined) {
 			let path = code.document.fileName.split(".");
-			path.pop();
-			path = path.join(".");
+				path.pop();
+				path = path.join(".");
+				path = path.replace(/\\/g, "\\\\");
 
 			output.show();
-			output.sendText(`"${ruota.path}" "${path}"`)
+			output.sendText(`"${ruota.path}" "${path}"`);
 		}
 	});
 
